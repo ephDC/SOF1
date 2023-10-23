f4031a26051790b29cbd7f6ec94a6edc77b1d499
ephDC <chimpopi.ephraim@gmail.com>
2023-10-23 20:21:53 +0200

    Entwicklung von Assembler

---
+++ src/org/hbrs/se1/ws23/uebung1/view/Assembler.java
@@ -0,0 +1,14 @@
+package org.hbrs.se1.ws23.uebung1.view;
+
+import org.hbrs.se1.ws23.uebung1.control.Translator;
+import org.hbrs.se1.ws23.uebung1.control.GermanTranslator;
+
+public class Assembler {
+    public static  void main(String[] args){
+        Translator translator = new GermanTranslator();
+        Client client = new Client(translator);
+        client.display(1);
+
+        //client.set
+    }
+}
