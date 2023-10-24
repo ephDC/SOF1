e9ceabbc0e526c9212961ad05c2fee85e35580e1
ephDC <chimpopi.ephraim@gmail.com>
2023-10-25 01:17:56 +0200

    Erstellung einer ContainerException

---
+++ src/org/hbrs/se1/ws23/uebung2/ContainerException.java
@@ -0,0 +1,8 @@
+package org.hbrs.se1.ws23.uebung2;
+
+public class ContainerException extends Exception {
+    //Konstruktor
+    public ContainerException(String msg){
+        super(msg);
+    }
+}
