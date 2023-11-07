6a2a730a0251797416610fa6fb3f0cb4a8d25045
ephDC <chimpopi.ephra@gmail.com>
2023-11-07 22:10:23 +0100

    Erstellung von Main Class.

---
+++ src/org/hbrs/se1/ws23/uebung3/persistence/Main.java
@@ -0,0 +1,6 @@
+package org.hbrs.se1.ws23.uebung3.persistence;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+public class Main {
+    Container a = Container.getContainer();
+}
