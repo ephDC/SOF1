fd734637b7c3841323c801988add00172596c4d8
ephDC <chimpopi.ephra@gmail.com>
2023-11-07 22:11:52 +0100

    Veränderung zum class Client um Singleton zu implementieren.

---
+++ src/org/hbrs/se1/ws23/uebung3/persistence/Client.java
@@ -0,0 +1,11 @@
+package org.hbrs.se1.ws23.uebung3.persistence;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Container container = Container.getContainer();
+    }
+
+}
